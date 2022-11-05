cb5e84becfbb16176fc54e557dcd50bc0f49c267
SIKI <117406014+IS4C4SI@users.noreply.github.com>
2022-11-06 01:16:23 +0300

    Create mod(en).lua

---
+++ Renaming names of difficulties for Payday 2/mod(en).lua
@@ -0,0 +1,15 @@
+{
+	"name" : "Renaming difficulties names",
+	"description" : "renames difficulties of contracts to custom",
+	"author" : "Sprin and SIKI",
+	"contact" : "https://is4c4si.tumblr.com",
+	"image" : "13245.png",
+	"color" : "10 10 10",
+	"version" : "1.0",
+	"hooks" : [
+		{
+			"hook_id" : "lib/managers/localizationmanager",
+			"script_path" : "difficulty.lua"
+		}
+	]
+}
