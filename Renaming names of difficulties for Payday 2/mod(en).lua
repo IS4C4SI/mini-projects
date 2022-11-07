b6f657fdac054858ec4468224326f2fa868cb406
SIKI <117406014+IS4C4SI@users.noreply.github.com>
2022-11-07 04:38:53 +0300

    Update mod(en).lua

--- Renaming names of difficulties for Payday 2/mod(en).lua
+++ Renaming names of difficulties for Payday 2/mod(en).lua
@@ -1,7 +1,7 @@
 {
 	"name" : "Renaming difficulties names",
 	"description" : "renames difficulties of contracts to custom",
-	"author" : "Sprin and SIKI",
+	"author" : "SIKI, Sprin, savvy and authors of WolfHud",
 	"contact" : "https://is4c4si.tumblr.com",
 	"image" : "13245.png",
 	"color" : "10 10 10",
